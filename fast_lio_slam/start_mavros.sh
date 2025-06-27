e90bf090bcc7d22ab5d3c12da3172719b9db6eaa
sanemildizz <sanemildiz@gmail.com>
2025-06-27 11:05:17 +0200

    updated rosbag info & corrected fcu_url

--- fast_lio_slam/start_mavros.sh
+++ fast_lio_slam/start_mavros.sh
@@ -3,4 +3,4 @@
 docker pull ghcr.io/mavtech-srl/mavros:0.1
 
 docker run -it --rm --privileged --network=host -v /dev:/dev ghcr.io/mavtech-srl/mavros:0.1 \
-    ros2 launch mavros px4.launch fcu_url:=/dev/ttyUSB:921600
+    ros2 launch mavros px4.launch fcu_url:=/dev/ttyUSB0:921600
