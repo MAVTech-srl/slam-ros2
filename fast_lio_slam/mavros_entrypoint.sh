08077e3f40db1373687bfaff2a6c7501eaa51f32
DavideCarminati <davide.carminati@outlook.com>
2025-10-15 14:38:32 +0200

    Updated to v0.6-dev. It uses our modified EKF for SLAM

--- fast_lio_slam/mavros_entrypoint.sh
+++ fast_lio_slam/mavros_entrypoint.sh
@@ -2,8 +2,10 @@
 
 #add sourcing to .bashrc
 echo "source /opt/ros/"$ROS_DISTRO"/setup.bash" >> /etc/bash.bashrc
+echo "source /usr/src/mavros_ws/install/local_setup.bash" >> /etc/bash.bashrc
 
 # setup ros2 environment
 source /opt/ros/"$ROS_DISTRO"/setup.bash --
+source /usr/src/mavros_ws/install/local_setup.bash --
 
 exec "$@"
--- fast_lio_slam/start_mavros.sh
+++ fast_lio_slam/start_mavros.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-# docker pull ghcr.io/mavtech-srl/mavros:0.1
+# Check number of arguments
+if [ "$#" -ne 2 ]; then
+    echo "[ start_mavros.sh ] Illegal number of parameters. It must be exactly 2"
+    exit 1
+fi
 
-docker run --rm --privileged --network=host -v /dev:/dev --name mavros ghcr.io/mavtech-srl/mavros:0.1 \
+docker run --rm --privileged --network=host -v /dev:/dev --name mavros ghcr.io/mavtech-srl/mavros:0.6-dev \
     ros2 launch mavros px4.launch fcu_url:=/dev/$1:$2
