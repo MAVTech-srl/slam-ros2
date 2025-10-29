697ad52e55b1f209b841068c342a4a20fcf047bb
DavideCarminati <davide.carminati@outlook.com>
2025-10-29 14:59:37 +0100

    Fixed wrong container version tag

--- fast_lio_slam/start_docker_slam_avia.sh
+++ fast_lio_slam/start_docker_slam_avia.sh
@@ -93,7 +93,7 @@ if [ "$PLATFORM" = "Raspberry" ]; then # Run Raspberry image
         --ipc=host \
         ${DOCKER_ARGS[@]} \
         --name fast-lio-slam \
-        ghcr.io/mavtech-srl/fast-lio-slam:0.6-rasp-dev ros2 launch --noninteractive src/slam_tools/launch/slam_avia.launch.py \
+        ghcr.io/mavtech-srl/fast-lio-slam:0.6.1-rasp-dev ros2 launch --noninteractive src/slam_tools/launch/slam_avia.launch.py \
               rviz:=$RVIZ_USE \
               save_pcd_cloud:=$LOCAL_PCD_USE \
               save_UTM_pcd_cloud:=$UTM_PCD_USE \
@@ -108,7 +108,7 @@ elif [ -f /etc/nv_tegra_release ]; then # Run Jetson docker image
         --ipc=host \
         ${DOCKER_ARGS[@]} \
         --name fast-lio-slam \
-        ghcr.io/mavtech-srl/fast-lio-slam:0.6-dev ros2 launch --noninteractive src/slam_tools/launch/slam_avia.launch.py \
+        ghcr.io/mavtech-srl/fast-lio-slam:0.6.1-dev ros2 launch --noninteractive src/slam_tools/launch/slam_avia.launch.py \
               rviz:=$RVIZ_USE \
               save_pcd_cloud:=$LOCAL_PCD_USE \
               save_UTM_pcd_cloud:=$UTM_PCD_USE \
